c40a20264b923acb27ecac5ad501dd584f4373b1
saitou-s-e-ize <sho.saito@e-ize.jp>
2020-12-02 09:14:13 +0900

    add laravel project

---
+++ laravel-circleci-demo2/tests/Feature/UserRegisterTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserRegisterTest extends TestCase
+{
+	use RefreshDatabase;
+    /**
+     * @test
+     */
+    public function ユーザー登録できる()
+    {
+        $email = 'email@example.com';
+        $this->post(route('register'), [
+            'name' => 'user',
+            'email' => $email,
+            'password' => 'password',
+            'password_confirmation' => 'password'
+        ])
+            ->assertStatus(302);
+        $this->assertDatabaseHas('users', ['email' => $email]);
+    }
+}
